a2ec71eae878bd751b0f7ec8fff332b7be47c813
Leandro Antunez <leandro_antunez@outlook.com>
2021-12-08 16:40:20 -0300

    Articles can be deleted.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,12 @@ class ArticlesController < ApplicationController
         redirect_to  @article
     end
 
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+
+        redirect_to root_path
+    end
 
 
     def new
