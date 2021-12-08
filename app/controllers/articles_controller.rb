d3224de88390bb1243f1a920df1d41138bd61761
Leandro Antunez <leandro_antunez@outlook.com>
2021-12-08 17:13:22 -0300

    Page to see all the articles.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
     before_action :find_article, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @articles = Article.all
+    end
     
     def show
     end
@@ -31,5 +35,7 @@ class ArticlesController < ApplicationController
     def find_article
         @article = Article.find(params[:id])
     end
+
+    
 
 end
