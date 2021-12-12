283c4e6aff370c27a59a3db8f13a3bd8797338db
Leandro Antunez <leandro_antunez@outlook.com>
2021-12-12 20:24:21 -0300

    Users now can edit a profile photo.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  resources :categories
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "bienvenida", to: "home#index"
-
+  get "perfil", to: "users#edit"
+  resource :users, only:[:update], as: :user
+  
   root to: "home#index"
-
+  
   get "articles/user/:user_id", to: "articles#from_author"
-
+  
+  resources :categories
   resources :articles do
     get "user/:user_id", to: "articles#from_author", on: :collection
   end
